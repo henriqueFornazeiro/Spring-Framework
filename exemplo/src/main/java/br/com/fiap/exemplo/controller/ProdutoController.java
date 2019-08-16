51f43228b510f5e44600612aa982ed84da357b73
Logon Aluno <logonaluno@local.com>
2019-08-16 11:37:38 -0300

    Aula inicial 16/08

---
+++ exemplo/src/main/java/br/com/fiap/exemplo/controller/ProdutoController.java
@@ -0,0 +1,21 @@
+package br.com.fiap.exemplo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ProdutoController {
+
+    @GetMapping("/produto/cadastrar")
+    public String abrirFormulario(){
+        return "/produto/form";
+    }
+
+    @PostMapping("/produto/cadastrar")
+     public String processarForm(Produto produto){
+        System.out.print(produto.getNome() + " " + produto.getPreco());
+        return "/produto/sucesso";
+    }
+
+}
