e5603a12084d31e14a9646ce7a9a7342f6b31a50
Logon Aluno <logonaluno@local.com>
2019-09-04 08:02:48 -0300

    Spring-Framework

--- springdatarest03/src/main/java/br/com/fiap/springrest03/ProdutoRepository.java
+++ springdatarest03/src/main/java/br/com/fiap/springrest03/ProdutoRepository.java
@@ -2,6 +2,8 @@ package br.com.fiap.springrest03;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProdutoRepository extends JpaRepository<Produto,Integer> {
-    
+
 }
